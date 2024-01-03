46566d3240f67488db5d18730b1f1d2a45b50ee8
M <yonasmillion29@gmail.com>
2024-01-04 00:29:09 +0300

    playground js file is made for javascript code exercise

---
+++ src/playground.js
@@ -0,0 +1,6 @@
+const fruit = ["Orange", "Banana", "Apple"];
+const vegitables = ["Cucumber", "Radish"];
+
+console.log([...fruit, ...vegitables]);
+
+// ... this symbole is used for merging of two array
