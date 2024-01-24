96e6b7bd65bcf31b97020168a15025bfcbf592d2
Yo <yonansmillion29@example.com>
2024-01-24 18:10:19 +0300

    playground.js file is updated

--- src/playground.js
+++ src/playground.js
@@ -4,3 +4,24 @@ const vegitables = ["Cucumber", "Radish"];
 console.log([...fruit, ...vegitables]);
 
 // ... this symbole is used for merging of two array
+
+console.log("================================================================");
+
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ["vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+
+developer.doubleSalary();
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
